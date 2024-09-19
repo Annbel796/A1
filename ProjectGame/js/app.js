d40bbd305c5487974965b76076ce0c9c85379c3a
Annbel796 <annbel796@ihmeducation.se>
2024-09-20 01:50:59 +0200

    Added Function

--- ProjectGame/js/app.js
+++ ProjectGame/js/app.js
@@ -1,39 +1,56 @@
-// Documents Elements
-// Looks "text0" and assign it to the variable called "textField0"
-const textField0 = document.getElementById("text0");
-const textField1 = document.getElementById("text1");
-
 //  Variables
-
-// Buttons
+  // Buttons
 let button0 = document.getElementById("button0");
 
-//Text
-let playerRollText = document.getElementById("PlayerRollText");
+  //Text
+let playerRollText = document.getElementById("playerRollText");
+let aiRollText = document.getElementById("aiRollText");
+let resultText = document.getElementById("result");
 
-//Data
+  //Data
 let playerRoll = 0;
+let aiRoll = 0;
 
 
 //Process
 button0.addEventListener("click", function () {
   getRandomNumberOneToSixForPlayer();
   showPlayerRollResult();
-
+  getRandomNumberOneToSixForAi();
+  showAiRollResult();
+  compareRolls();
 });
 
 // Controllers
 function getRandomNumberOneToSixForPlayer(){
   playerRoll = Math.floor(Math.random() * 6) + 1;
-  console.log (playerRoll)
+}
 
+function getRandomNumberOneToSixForAi(){
+  aiRoll = Math.floor(Math.random() * 6) + 1;
+}
+
+function compareRolls() {
+  if (playerRoll > aiRoll) {
+    showText("Winner Player");
+  } else if (playerRoll < aiRoll) {
+    showText("Winner AI");
+  } else {
+    showText("Draw");
+  }
 }
 
 //View
 function showPlayerRollResult(){
-  playerRollText.innerHTML = playerRoll;
+  playerRollText.innerHTML = "Player: " + playerRoll;
 }
 
+function showAiRollResult(){
+ aiRollText.innerHTML = "AI: " + aiRoll;
+}
 
+function showText(result) {
+  resultText.innerHTML = result;
+}
 
 
