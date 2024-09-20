8d661edaadeff355f967e8d4994d0ef887176875
Annbel796 <annbel796@ihmeducation.se>
2024-09-20 10:34:25 +0200

    Added Final Function

--- ProjectGame/js/app.js
+++ ProjectGame/js/app.js
@@ -3,13 +3,19 @@
 let button0 = document.getElementById("button0");
 
   //Text
+const textField0 = document.getElementById("text0");
+const textField1 = document.getElementById("text1");
 let playerRollText = document.getElementById("playerRollText");
 let aiRollText = document.getElementById("aiRollText");
 let resultText = document.getElementById("result");
+let totalScoreText = document.getElementById("totalScoreText");
 
   //Data
 let playerRoll = 0;
 let aiRoll = 0;
+let playerScore = 0;
+let aiScore = 0;
+let drawScore = 0;
 
 
 //Process
@@ -19,6 +25,7 @@ button0.addEventListener("click", function () {
   getRandomNumberOneToSixForAi();
   showAiRollResult();
   compareRolls();
+  showAllResult();
 });
 
 // Controllers
@@ -32,10 +39,13 @@ function getRandomNumberOneToSixForAi(){
 
 function compareRolls() {
   if (playerRoll > aiRoll) {
-    showText("Winner Player");
+    playerScore++;
+    showText("Player wins this round!");
   } else if (playerRoll < aiRoll) {
-    showText("Winner AI");
+    aiScore++;
+    showText("AI wins this round!");
   } else {
+    drawScore++;
     showText("Draw");
   }
 }
@@ -53,4 +63,9 @@ function showText(result) {
   resultText.innerHTML = result;
 }
 
+function showAllResult(){
+  totalScoreText.innerHTML = "Total score: Player - " + playerScore + "; AI - " + aiScore + "; Draw - " + drawScore;
+}
 
+textField0.style.color = "red";
+button0.style.color = "red";
